49d568369fa3e5b8d873ea10514cb3df7c6ee322
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-10 09:21:27 +0000

    [4-25]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root to: "homes#top"
+
+  get "homes/about" => "homes#about", as: "about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
